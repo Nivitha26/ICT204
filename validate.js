cf7ed07ece830cc7098cca5ce79756c59f6ea336
Nivitha Rajendran <rajnivi2017@gmail.com>
2022-01-11 16:05:22 +0530

    Final Commit

---
+++ validate.js
@@ -0,0 +1,11 @@
+<script language="Javascript">
+    function IsEmpty() {
+if (document.form.stdName.value == "") {
+        alert("empty");
+      }
+      if (document.form.stdRegNum.value == "") {
+        alert("empty");
+      }
+      return;
+    }
+  </script>
